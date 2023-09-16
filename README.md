144d2e18e680730a9aba0b766196167b46f913bb
aquajr <ayandaobosi@gmail.com>
2023-09-16 10:39:02 +0300

    login

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Sample application using Python and Flask
 - Project is a test on how to start and run a flask server
 - You will need to install a few things before trying it out
+- Added a login form
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
 from app import app
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def index():
-    return 'Hello world'
+    return render_template('index.html', title='Index Page')
+
+
+@app.route('/about-me')
+def about_me():
+    return render_template('about_me.html', title='About Me Page')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form =LoginForm()
+    if form.validate_on_submit():
+        flash(f'You are requesting to login in {form.username.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Login', form=form)
